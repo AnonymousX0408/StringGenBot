a2f5339ed495986af4e8053192c5586779ea3416
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <142774957+AnonymousX0408@users.noreply.github.com>
2024-02-18 10:22:39 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -1,27 +1,20 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
 from config import SUPPORT_CHAT
-
 
 keyboard = InlineKeyboardMarkup(
     [
         [InlineKeyboardButton(text="🐣 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🐣", callback_data="gensession")],
         [
             InlineKeyboardButton(text="📢 ᴜᴘᴅᴀᴛᴇꜱ", url=SUPPORT_CHAT),
-            InlineKeyboardButton(
-                text="💸 ᴅᴇᴠ", url="https://telegram.me/Mr_RoleXG"
-            ),
+            InlineKeyboardButton(text="💸 ᴅᴇᴠ", url="https://telegram.me/Mr_RoleXG"),
         ],
     ]
 )
 
 gen_key = InlineKeyboardMarkup(
     [
-        [
-            InlineKeyboardButton(text="🔮 ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1")],
-    ],
-            [InlineKeyboardButton(text="🔮 ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram")],
-        ],
+        [InlineKeyboardButton(text="🔮 ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1")],
+        [InlineKeyboardButton(text="🔮 ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram")],
         [InlineKeyboardButton(text="🔮 ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
     ]
 )
